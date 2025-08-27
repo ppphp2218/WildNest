f822afac301b9abf6137ff6aae6b6f3f4f37cf83
ppphp2218 <lutengyu@outlook.com>
2025-08-27 23:47:42 +0800

    添加Spring Boot后端项目框架和依赖配置

---
+++ WildNestBack/src/main/java/com/HuangYe/WildNest/WildNestApplication.java
@@ -0,0 +1,13 @@
+package com.HuangYe.WildNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildNestApplication.class, args);
+	}
+
+}
